0c937d3bc3aae9e2fd3569ff055a2f863c7c3a53
TuhinCodeCraft <tuhinghosh0169@gmail.com>
2024-07-16 17:23:09 +0530

    chore: Add dependencies for CORS and cookie-parser

--- src/index.js
+++ src/index.js
@@ -6,20 +6,34 @@ dotenv.config({
     path: './env'
 }) // to use this we need to add experimental feature in script in package.json
 
-connectDB()
+connectDB() // it will return a promise
+.then(() => {
+    app.on("error", (error) => {
+        console.log(`ERROR: ${error}`);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on the port: ${process.env.PORT}`);
+    })
+}) 
+.catch((error) => {
+    console.log("MONGODB CONNECTION FAILED!!!: ", error);
+})
 
 
 
 
 
 /*
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
 import express from "express";
 const app = express();
 
 // if some body forget to put ; at the end of the previous line
 ;( async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // Database name is mandatory
         app.on("error", (error) => {
             console.error("Error: ", error);
             throw error;
@@ -33,3 +47,5 @@ const app = express();
     }
 })() 
 */
+
+// To communicate with the database we need to install mongoose package and you have to use async await to connect to the database cause it is in another continent and it will take time to connect to the database and we need to wait for it to connect to the database and then we can start the server and wrap it under try catch block to catch the error if there is any error while connecting to the database
