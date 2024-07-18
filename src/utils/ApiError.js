033c2a7f1adfaf8a04c361a4936d22f8852642bd
TuhinCodeCraft <tuhinghosh0169@gmail.com>
2024-07-18 21:46:22 +0530

    Adding Data Models

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -15,7 +15,7 @@ class ApiError extends Error {
         if(stack){
             this.stack = stack
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor) // It allows you to capture the current stack trace at the point where the method is called. A stack trace is a list of function calls that shows the sequence of function invocations that led to the current point in the code. Here, the this keyword refers to the current instance of the object, and this.constructor refers to the constructor function of the object.
         }
     }
 }
